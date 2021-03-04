2f7394e4c51712b91c995ea2b4435dc3cde55dab
tebanban <estebancalf@gmail.com>
2021-03-04 18:29:32 +0000

    added footer

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,7 +3,11 @@ import React from "react";
 export function Card() {
 	return (
 		<div className="card" style={{ width: "14rem" }}>
-			<img className="card-img-top" src="..." alt="Card image cap" />
+			<img
+				className="card-img-top"
+				src="https://picsum.photos/70/40"
+				alt="Card image cap"
+			/>
 			<div className="card-body">
 				<h5 className="card-title">Card title</h5>
 				<p className="card-text">
