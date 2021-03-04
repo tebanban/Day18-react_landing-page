db9fb3ed882f7e84e921fcc60ce56fac765621bb
tebanban <estebancalf@gmail.com>
2021-03-04 04:51:39 +0000

    navbar y jumbotron

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 //import your own components
+import { Navbar } from "./component/navbar";
 import { Jumbotron } from "./component/jumbotron";
 
 export function App() {
 	return (
 		<div className="text-center">
+			<Navbar />
 			<Jumbotron />
 		</div>
 	);
