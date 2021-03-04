fd9e5a3e17dbbcc68397ab2feac8ff2c601e9a5c
tebanban <estebancalf@gmail.com>
2021-03-04 01:57:15 +0000

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//import your own components
+import { Jumbotron } from "./component/jumbotron";
+
+export function App() {
+	return (
+		<div className="text-center">
+			<Jumbotron />
+		</div>
+	);
+}
